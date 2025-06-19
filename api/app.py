6bd39f216da36dc14c8979be18f77e2c81e2bf03
yurah25 <yusufrafiiahmad25@gmail.com>
2025-06-19 14:05:20 +0700

    M

--- api/app.py
+++ api/app.py
@@ -2,7 +2,11 @@ from flask import Flask, render_template, request, abort
 import os
 from datetime import datetime
 
-app = Flask(__name__)
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+template_dir = os.path.join(project_root, 'templates')
+static_dir = os.path.join(project_root, 'static')
+
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 
 KOST_DATA = [
     {
